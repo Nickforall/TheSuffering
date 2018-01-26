3cf02326d9fd86fd56aabbb666e67c3dbffde34d
boltgolt <boltgolt@gmail.com>
2018-01-26 22:00:13 +0100

    No spaces pls

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 function render() {
-    // This is the game loop, and is executed every frame.
-    window.requestAnimationFrame(render);
+	// This is the game loop, and is executed every frame.
+	window.requestAnimationFrame(render);
 
-    // make pixy rerender
-    RENDERER.render();
+	// make pixy rerender
+	RENDERER.render();
 }
 
 // This is called when the window is ready loading
 window.addEventListener('DOMContentLoaded', () => {
-    // add stuff to the global scope (RENDERER can now be accessed from anywhere)
-    window.RENDERER = new PIXI.Application(800, 600, {
-        backgroundColor : 0x111111
-    });
+	// add stuff to the global scope (RENDERER can now be accessed from anywhere)
+	window.RENDERER = new PIXI.Application(800, 600, {
+		backgroundColor : 0x111111
+	});
 
-    // add canvas to document
-    document.body.appendChild(RENDERER.view);
+	// add canvas to document
+	document.body.appendChild(RENDERER.view);
 
-    console.log(' > We loaded!');
+	console.log(' > We loaded!');
 
-    // start game loop
-    window.requestAnimationFrame(render);    
+	// start game loop
+	window.requestAnimationFrame(render);	
 });
