a639f794d095c0bc600bf01486e5a8c0fc6c58a0
boltgolt <boltgolt@gmail.com>
2018-01-26 22:51:29 +0100

    Added top (TAPP) and bottom (BAPP) contexts

--- src/index.js
+++ src/index.js
@@ -2,22 +2,37 @@ function render() {
 	// This is the game loop, and is executed every frame.
 	window.requestAnimationFrame(render);
 
-	// make pixy rerender
-	RENDERER.render();
+	// Make pixy rerender
+	TAPP.render();
+	BAPP.render();
 }
 
 // This is called when the window is ready loading
-window.addEventListener('DOMContentLoaded', () => {
-	// add stuff to the global scope (RENDERER can now be accessed from anywhere)
-	window.RENDERER = new PIXI.Application(800, 600, {
-		backgroundColor : 0x111111
-	});
+window.addEventListener("DOMContentLoaded", () => {
+	/**
+	 * Start a new application context
+	 * @param  {String} global The name of the global to store the new application in
+	 */
+	function initApp(global) {
+		// add stuff to the global scope (APP can now be accessed from anywhere)
+		window[global] = new PIXI.Application(window.innerWidth, window.innerHeight / 2, {
+			backgroundColor: 0x111111,
+			forceCanvas: true
+		});
 
-	// add canvas to document
-	document.body.appendChild(RENDERER.view);
+		// Let the context resize
+		window[global].renderer.autoResize = true;
 
-	console.log(' > We loaded!');
+		// Add it to the DOM
+		document.body.appendChild(window[global].view);
+	}
 
-	// start game loop
-	window.requestAnimationFrame(render);	
+	// Start both the top and the bottom app
+	initApp("TAPP")
+	initApp("BAPP")
+
+	console.debug("2 applications loaded");
+
+	// Start game loop
+	window.requestAnimationFrame(render);
 });
