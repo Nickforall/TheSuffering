0d60e624b86c786bccea94adc5bcc627f002afc7
RoelVoordendag <rvoordendag@gmail.com>
2018-01-27 13:57:48 +0100

    added base buff and debuff

---
+++ src/equipment/buff.js
@@ -0,0 +1,35 @@
+import buff from '../../resources/buff.json'
+export default class Buffs {
+    constructor() {
+
+    }
+    //this function is to choose a random buff 
+    randomBuff() {
+        
+        //getting random number for the buffs
+        let id = Math.floor((Math.random() * buff.length) + 1) -1;
+
+        //checking which function to actived
+        switch(buff[id].name){
+            case 'gun':
+                this.gun();
+            break;
+            case 'oneHit' :
+                this.oneHit();
+            break;
+            case 'noDamage' :
+                this.noDamage();
+            break;
+        }
+        
+    }
+    gun(){
+        console.log('gun')
+    }
+    oneHit(){
+        console.log('one hit')
+    }
+    noDamage(){
+        console.log('no damage')
+    }
+}
---
+++ src/equipment/debuff.js
@@ -0,0 +1,35 @@
+import debuff from '../../resources/debuff'
+export default class deBuff{
+    constructor(){
+
+    }
+    //this function is to choose a random debuff
+    randomDeBuff() {
+        
+        //getting random number for the buffs
+        let id = Math.floor((Math.random() * debuff.length) + 1) -1;
+
+        //checking which function to actived
+        switch(debuff[id].name){
+            case 'noJump':
+                this.gun();
+            break;
+            case 'slowing' :
+                this.slowing();
+            break;
+            case 'freeze' :
+                this.freeze();
+            break;
+        }
+    }
+    noJump(){
+        console.log('no jump')
+    }
+    slowing(){
+        console.log('slowing')
+    }
+    freeze(){
+        console.log('freeze')
+    }
+}
+
